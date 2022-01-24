5c56db7c305e0987f69996d833f68ab262c58978
gio_barranco <sandiego_94@hotmail.com>
2022-01-24 08:01:52 -0700

    comments on js & local storage added to t-blocks

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## Description
+* The “Work Day Scheduler” web page contains a day calendar where the user is able to input their tasks for the day during normal business hours. The tasks are organized by hour-blocks that are color coded based on what time of day it is. If the user input is in a current hour block the color in the hour-block will be green, the hour-blocks in the past will be grey and the hour-blocks in the future will be red.
+
+## Technologies Used
+* Html and CSS for structuring and styling the webpage
+* Javascript - jquery/moment
--- script.js
+++ script.js
@@ -7,47 +7,33 @@ $(document).ready(function () {
         //the input of the user will be saved in the var text
         //val() returns the value of the user input
         var text = $(this).siblings(".description").val();
-
         var time = $(this).parent().attr("id");
-        //text and time from user input is saved to their computer 
+        //text and time from when user inputs text, is saved to their computer 
         localStorage.setItem(time, text);
     })
- })
-
-var currentTime = moment().hours();
 
 
-$( ".time-block" ).each(function() {
-   var hour =  $( this ).attr("id");
-   hour = parseInt(hour.split("-")[1]); 
-   console.log(hour);
-   
-   if (currentTime === hour) {
-    $(this).find(".description").addClass( "present" );
-   } else if (currentTime < hour) {
-       $(this).find(".description").addClass("past");
-   } else {
-    $(this).find(".description").addClass("future");
-   }
+    var currentTime = moment().hours();
 
+//looping through all the time blocks
+//takes the number in hour-block string and sets
+//the variable hour to just the number
+    $(".time-block").each(function () {
+        var hour = $(this).attr("id");
+        hour = parseInt(hour.split("-")[1]);
+        //compares the current time to past, future & present time-blocks
+        //to appropriately color code the blocks
+        if (currentTime === hour) {
+            $(this).find(".description").addClass("present");
+        } else if (currentTime < hour) {
+            $(this).find(".description").addClass("past");
+        } else {
+            $(this).find(".description").addClass("future");
+        }
 
-
-  });
-   
-  // Instead, you should rely on implicit iteration:
- // $( "li" ).addClass( "bar" );
-
-
-
- //local storage: storage in user's computer
- //saving the time and text the user inputs into their computer
- //val()- https://api.jquery.com/val/
-
- //compared times to past, present and future with else/if statements
- //make a time variable that will be compared with the current time
- //past, future, present are classes 
- //appending the classes to div element based on the time comparison -APPEND to classes
- //when user refreshes page, the input is still there using localStorage
- //get the values from localStorage and put it on the page if they saved the input
- //run the fucntion
-
+        //getting the value from local Storage and setting it equal
+        //to the variale hourID
+        var hourID = $(this).attr("id");
+        $(hourID.description).val(localStorage.getItem(hourID));
+    });
+})
